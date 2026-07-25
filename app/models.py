62dbfaf37b93dc2f712e54ad0f029213f125baec
VargasAnthony <na242967@gmail.com>
2026-07-25 12:34:38 -0500

    Modelos de usuaris y tareas con relaciones

---
+++ app/models.py
@@ -0,0 +1,23 @@
+from sqlalchemy import Column, String, Integer, Float, Boolean, ForeignKey
+from sqlalchemy.orm import relationship
+from .database import Base
+
+class Usuario(Base):
+    __tablename__ = "Usuarios"
+
+    id = Column(Integer, primary_key = True, index = True)
+    email = Column(String, unique=True, nullable=False)
+    hashed_password = Column(String, nullable=False)
+
+    tareas = relationship("Tarea", back_populates="owner")
+
+class Tarea(Base):
+    __tablename__ = "Tareas"
+
+    id = Column(Integer, primary_key=True, index = True)
+    titulo = Column(String, nullable = False)
+    description = Column(String, nullable = True)
+    completada = Column(Boolean, default = False)
+    usuario_id = Column(ForeignKey("Usuarios.id"), nullable = False)
+
+    owner = relationship("Usuario", back_populates="tareas")
