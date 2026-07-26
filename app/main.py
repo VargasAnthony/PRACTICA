173155082d7953f1b5ac3c07fba3a527c57781b4
VargasAnthony <na242967@gmail.com>
2026-07-26 14:19:15 -0500

    main.py: conexion de routers y creacion de tablas

---
+++ app/main.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+from database import Base, engine
+from routers import users, tasks
+
+app = FastAPI(
+    title="Mi API con FastAPI",
+    description="API inicial para la primera prueba real",
+    version="1.0.0"
+)
+
+Base.metadata.create_all(bind=engine)
+
+app.include_router(tasks.router)
+app.include_router(users.router)
+
+
+@app.get("/")
+def read_root():
+    return {"mensaje": "¡Servidor FastAPI corriendo con éxito!"}
