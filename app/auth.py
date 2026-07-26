d4b90f1ad497061d801cd3ee981e6450af8e64eb
VargasAnthony <na242967@gmail.com>
2026-07-25 21:10:09 -0500

    Funciones de generacion y verificacion de JWT

--- app/auth.py
+++ app/auth.py
@@ -1,5 +1,11 @@
 from passlib.context import CryptContext
+from jose import jwt, JWTError
+from datetime import datetime, timedelta
 
+
+SECRET_KEY = "a1b2c3d4e5f67890a1b2c3d4e5f67890"
+ALGORITHM = "HS256"
+ACCES_TOKEN_EXPIRE_MINUTES = 60
 
 pwd_context = CryptContext(
     schemes=["bcrypt"],
@@ -11,3 +17,18 @@ def hash_password(password: str):
 
 def verify_password(plain_password: str, hashed_password: str):
     return pwd_context.verify(plain_password, hashed_password)
+
+def create_acces_token(data: dict):
+    to_encode = data.copy()
+    expire = datetime.utcnow() + timedelta(minutes=ACCES_TOKEN_EXPIRE_MINUTES)
+    to_encode["exp"] = expire
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+
+    return encoded_jwt
+
+def verify_access_token(token:str):
+    try:
+        decode_jwt = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        return decode_jwt
+    except JWTError:
+        return None
